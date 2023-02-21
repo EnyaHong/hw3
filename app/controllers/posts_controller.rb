697a42d411be1f745e7a5d278765be610f758cc1
EnyaHong <enya.hong@kellogg.northwestern.edu>
2023-02-21 03:18:39 +0000

    attempt5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,17 +4,23 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find_by({ "id" => params["id"] })
+    @place = Place.find_by({ "id" => @contact["place_id"] })
+  end
+
   def new
     @post = Post.new
+    @post["place_id"] = params["place_id"]
   end
   
   def create
     @post = Post.new
     @post["title"] = params["post"]["title"]
-    @post["long_description"] = params["post"]["long_description"]
-    @post["date"] = params["post"]["date"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
     @post.save
-    redirect_to "/posts"
+    redirect_to "/places/#{@post["place_id"]}"
   end 
 
 
